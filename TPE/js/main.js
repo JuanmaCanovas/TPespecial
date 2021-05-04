6c3347ff74981ef5e8f3a4b639fb8e341ab557d3
JuanmaCanovas <juanmacanovas13@gmail.com>
2021-05-04 07:40:10 -0300

    Se acomodo codigo js de portfolio

--- TPE/js/main.js
+++ TPE/js/main.js
@@ -1,12 +1,13 @@
-document.getElementById("work1").addEventListener("mouseover",()=>{
-    let img =document.getElementById("work1");
+document.getElementById("work1").addEventListener("mouseover", cambiarPastWork1);
+function cambiarPastWork1 (){
+    let img = document.getElementById("work1");
     img.src = "assets/pastworks1hov.PNG";
-});
-
-document.getElementById("work1").addEventListener("mouseout",()=>{
+}
+document.getElementById("work1").addEventListener("mouseout", original1);
+function original1 (){
     let img = document.getElementById("work1");
     img.src = "assets/pastworks1.PNG";
-});
+}
 
 document.getElementById("work2").addEventListener("mouseover", cambiarPastWork2);
 function cambiarPastWork2 (){
@@ -18,13 +19,16 @@ function original2 (){
     let img = document.getElementById("work2");
     img.src = "assets/pastworks2.PNG";
 }
-
-document.getElementById("work3").addEventListener("mouseover", ()=>
-    document.getElementById("work3").src="assets/pastworks3hov.PNG"
-)
-document.getElementById("work3").addEventListener("mouseout", ()=>
-    document.getElementById("work3").src="assets/pastworks3.PNG"
-)
+document.getElementById("work3").addEventListener("mouseover", cambiarPastWork3);
+function cambiarPastWork3 (){
+    let img = document.getElementById("work3");
+    img.src = "assets/pastworks3hov.PNG";
+}
+document.getElementById("work3").addEventListener("mouseout", original3);
+function original3 (){
+    let img = document.getElementById("work3");
+    img.src = "assets/pastworks3.PNG";
+}
 
 document.getElementById("work4").addEventListener("mouseover", cambiarPastWork4);
 function cambiarPastWork4 (){
